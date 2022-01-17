6f71c42769c4687ae02816df7c592f39bc3ffcef
Jardel <jardel1101@gmail.com>
2022-01-17 00:15:53 -0300

    Ajuste no padrão de nomes dos arquivos de stories e ajuste no jest.config.js para ignorar eles.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom', // como vamos testar o front, no browser, simular a dom
   testPathIgnorePatterns: ['/node_modules', '/.next/', '/.husky/'], // dirs ignorados
   collectCoverage: true, // fazer teste de cobertura
-  collectCoverageFrom: ['src/**/*.ts(x)'], // dir de onde será coletada a cobertura
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?', // de onde deve ser coletado
+    '!src/**/@(*.|)stories.@(js|jsx|ts|tsx)' // esses devem ser ignorados
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'] // arquivos carregados antes dos testes
 }
